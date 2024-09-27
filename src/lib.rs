8afe684e1872d1a9a69e5ef8f4d259191e76f888
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-09-27 22:21:59 +1000

    basic text rendering

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,41 @@
-use macroquad::ui::root_ui;
-use macroquad::window::clear_background;
+use macroquad::prelude::*;
+use crate::Message::{Char, Keypress};
 use crate::theme::Theme;
 
 pub mod theme;
 
 pub struct App {
     theme: Theme,
-    count: u64
+    lines: Vec<String>,
+    cursor_position: (usize, usize), // line, character
+    pub font: Option<Font>,
+    pub font_size: u16
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
             theme: Theme::mocha(),
-            count: 0
+            lines: vec!["hello".to_owned(), "world".to_owned()],
+            cursor_position: (0, 2),
+            font: None,
+            font_size: 16
         }
     }
 }
 
 pub enum Message {
-    Increment
+    CursorMove(usize, usize),
+    Keypress(KeyCode),
+    Char(char),
 }
 
 impl App {
+
     pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => self.count += 1,
-        }
+        // match message {
+        //
+        // }
     }
 
     pub fn view(&self) -> Vec<Message> {
@@ -34,9 +43,36 @@ impl App {
 
         clear_background(self.theme.base);
 
-        root_ui().label(None, "hello megaui");
-        if root_ui().button(None, format!("push me {}", self.count)) {
-            messages.push(Message::Increment);
+        let mut x = 0.0;
+        let mut y = 20.0;
+
+
+        for (i, line) in self.lines.iter().enumerate() {
+            for (j, glyph) in line.chars().enumerate() {
+                let dimensions = draw_text_ex(&glyph.to_string(), x, y, TextParams {
+                    color: self.theme.text,
+                    font: self.font.as_ref(),
+                    font_size: self.font_size,
+                    ..Default::default()
+                });
+                x += dimensions.width;
+
+                if (i, j) == self.cursor_position {
+                    draw_rectangle(x, y, 2.0, self.font_size as f32, self.theme.text)
+                }
+
+            }
+            x = 0.0;
+            y += self.font_size as f32;
+        }
+
+        // Input handling
+        for key in get_keys_pressed() {
+            messages.push(Keypress(key));
+        }
+
+        if let Some(character) = get_char_pressed() {
+            messages.push(Char(character))
         }
 
         messages
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use benchide::App;
 use macroquad::prelude::*;
-
 #[macroquad::main("benchide")]
 async fn main() {
-    let mut state = App::default();
+    let mut app = App::default();
+
+    let font = load_ttf_font("./JetBrainsMono-Regular.ttf").await.unwrap();
+
+    app.font = Some(font);
 
     loop {
-        let messages = state.view();
+        let messages = app.view();
 
         for message in messages {
-            state.update(message);
+            app.update(message);
         }
 
         next_frame().await
