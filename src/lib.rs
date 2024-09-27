f806063bd5bc7069e50a67ff2c8bfd5f168efdbb
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-09-27 23:22:41 +1000

    some text editing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use macroquad::prelude::*;
-use crate::Message::{Char, Keypress};
 use crate::theme::Theme;
 
 pub mod theme;
@@ -19,23 +18,59 @@ impl Default for App {
             lines: vec!["hello".to_owned(), "world".to_owned()],
             cursor_position: (0, 2),
             font: None,
-            font_size: 16
+            font_size: 40
         }
     }
 }
 
 pub enum Message {
-    CursorMove(usize, usize),
     Keypress(KeyCode),
     Char(char),
 }
 
 impl App {
 
+    fn move_cursor(&mut self, key: KeyCode) {
+        use KeyCode::*;
+        match key {
+            Up => {
+                if self.cursor_position.0 > 0 {
+                    self.cursor_position.0 -= 1
+                }
+            }
+            Left => {
+                if self.cursor_position.1 > 0 {
+                    self.cursor_position.1 -= 1
+                }
+            }
+            Right => self.cursor_position.1 += 1,
+            Down => self.cursor_position.0 += 1,
+            _ => {}
+        }
+
+        println!("{}", self.lines.len());
+        if self.cursor_position.0 >= self.lines.len() {
+            self.cursor_position.0 = self.lines.len() - 1
+        }
+
+        if self.cursor_position.1 >= self.lines[self.cursor_position.0].len() {
+            self.cursor_position.1 = self.lines[self.cursor_position.0].len()
+        }
+
+        println!("{:?}", self.cursor_position)
+    }
+
     pub fn update(&mut self, message: Message) {
-        // match message {
-        //
-        // }
+        match message {
+            Message::Keypress(key) => self.move_cursor(key),
+
+            Message::Char(character) => {
+                let (y, x) = self.cursor_position;
+
+                self.lines[y].insert(x, character);
+                self.cursor_position.1 += 1;
+            }
+        }
     }
 
     pub fn view(&self) -> Vec<Message> {
@@ -44,35 +79,41 @@ impl App {
         clear_background(self.theme.base);
 
         let mut x = 0.0;
-        let mut y = 20.0;
+        let mut y = self.font_size as f32;
 
 
         for (i, line) in self.lines.iter().enumerate() {
             for (j, glyph) in line.chars().enumerate() {
+                if (i, j) == self.cursor_position {
+                    draw_rectangle(x, y - self.font_size as f32, 2.0, self.font_size as f32, self.theme.text)
+                }
+
                 let dimensions = draw_text_ex(&glyph.to_string(), x, y, TextParams {
                     color: self.theme.text,
                     font: self.font.as_ref(),
                     font_size: self.font_size,
+                    font_scale: 1.0,
                     ..Default::default()
                 });
+
                 x += dimensions.width;
-
-                if (i, j) == self.cursor_position {
-                    draw_rectangle(x, y, 2.0, self.font_size as f32, self.theme.text)
-                }
-
             }
+
+            if (i, line.len()) == self.cursor_position {
+                draw_rectangle(x, y - self.font_size as f32, 2.0, self.font_size as f32, self.theme.text)
+            }
+
             x = 0.0;
             y += self.font_size as f32;
         }
 
         // Input handling
         for key in get_keys_pressed() {
-            messages.push(Keypress(key));
+            messages.push(Message::Keypress(key));
         }
 
         if let Some(character) = get_char_pressed() {
-            messages.push(Char(character))
+            messages.push(Message::Char(character))
         }
 
         messages
