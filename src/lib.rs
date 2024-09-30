2a92ee8d0c692e0cd4a8d5098cf5e03ef0f378a9
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-09-30 14:12:31 +1000

    feat: draw command prompt

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use inkjet::Highlighter;
 use macroquad::prelude::*;
 use uuid::Uuid;
 use crate::editor::{Editor, EditorMessage};
+use crate::prompt::{Prompt, PromptUpdate};
 use crate::theme::Theme;
 
 pub mod theme;
@@ -17,7 +18,9 @@ pub struct App {
     highlighter: Highlighter,
     focused: Option<Uuid>,
     move_target: Option<Uuid>,
-    prompt_focused: bool
+    prompt_focused: bool,
+    released: bool,
+    prompt: Prompt
 }
 
 impl Default for App {
@@ -34,7 +37,9 @@ impl Default for App {
             highlighter: Highlighter::new(),
             focused: None,
             move_target: None,
-            prompt_focused: false
+            prompt_focused: false,
+            released: true,
+            prompt: Prompt::new()
         }
     }
 }
@@ -45,12 +50,14 @@ pub enum Message {
     Focus(Option<Uuid>),
     Scroll(Uuid, Vec2),
     Pan(Vec2),
-    MoveTarget(Option<Uuid>)
+    MoveTarget(Option<Uuid>),
+    FocusPrompt(bool),
+    KeyComboDone,
+    PromptEdit(EditorMessage)
 }
 
 impl App {
     pub fn update(&mut self, message: Message) {
-        println!("{:?}", message);
         match message {
             Message::Focus(uuid) => self.focused = uuid,
             Message::Edit(uuid, edit) =>
@@ -70,6 +77,23 @@ impl App {
             Message::MoveTarget(target) => {
                 self.move_target = target
             }
+            Message::FocusPrompt(focused) => {
+                if self.released {
+                    self.prompt_focused = focused;
+                }
+                self.released = false;
+            }
+            Message::KeyComboDone => {
+                self.released = true;
+            }
+            Message::PromptEdit(msg) => {
+                if let Some(update) =  self.prompt.update(msg) {
+                    match update {
+                        _ => {}
+                    }
+                    self.prompt_focused = false;
+                }
+            }
         }
     }
 
@@ -80,6 +104,30 @@ impl App {
     }
 
     fn handle_input(&self, messages: &mut Vec<Message>) {
+        if get_keys_down().contains(&KeyCode::K)
+            && (get_keys_down().contains(&KeyCode::LeftControl)
+            || get_keys_down().contains(&KeyCode::RightControl)) {
+            messages.push(Message::FocusPrompt(!self.prompt_focused));
+            if !self.prompt_focused {
+                return;
+            }
+        } else {
+            if !self.released {
+                messages.push(Message::KeyComboDone);
+            }
+            if self.prompt_focused {
+                for key in get_keys_pressed() {
+                    messages.push(Message::PromptEdit(EditorMessage::Keypress(key)));
+                }
+
+                if let Some(character) = get_char_pressed() {
+                    messages.push(Message::PromptEdit(EditorMessage::Char(character)));
+                }
+
+                return;
+            }
+        }
+
         let hovered_editor = self.editors.iter()
             .find(|(_, editor)|
                 editor.window.contains(Vec2::from(mouse_position()))
@@ -123,6 +171,8 @@ impl App {
                 messages.push(Message::Edit(uuid, EditorMessage::Char(character)));
             }
         }
+
+        clear_input_queue();
     }
 
     pub fn view(&self) -> Vec<Message> {
@@ -134,6 +184,10 @@ impl App {
 
         for (uuid, editor) in &self.editors {
             editor.view(&self.theme, self.font.as_ref(), self.focused == Some(*uuid));
+        }
+
+        if self.prompt_focused {
+            self.prompt.view(&self.theme, self.font.as_ref())
         }
 
         self.handle_input(&mut messages);
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,12 +1,14 @@
 use macroquad::prelude::*;
 use crate::editor::EditorMessage;
+use crate::theme::Theme;
+use crate::window::set_fullscreen_camera;
 
-pub(crate) struct Command {
+pub(crate) struct Prompt {
     text: String,
     cursor: usize,
 }
 
-pub(crate) enum CommandUpdate {
+pub(crate) enum PromptUpdate {
     OpenFile(String),
     CloseActiveFile,
     SaveActiveFile,
@@ -15,7 +17,7 @@ pub(crate) enum CommandUpdate {
     SaveAs(String),
 }
 
-impl Command {
+impl Prompt {
     pub(crate) fn new() -> Self {
         Self {
             text: String::from(""),
@@ -23,7 +25,7 @@ impl Command {
         }
     }
 
-    pub(crate) fn update(&mut self, message: EditorMessage) -> Option<CommandUpdate> {
+    pub(crate) fn update(&mut self, message: EditorMessage) -> Option<PromptUpdate> {
         match message {
             EditorMessage::Keypress(key) => {
                 match key {
@@ -47,11 +49,14 @@ impl Command {
                         update
                     }
                     '\x08' => {
-                        self.text.remove(self.cursor - 1);
+                        if self.cursor > 0 {
+                            self.text.remove(self.cursor - 1);
+                        }
                         None
                     }
                     key => {
                         self.text.insert(self.cursor, key);
+                        self.cursor += 1;
                         None
                     }
                 }
@@ -59,9 +64,42 @@ impl Command {
         }
     }
 
-    pub(crate) fn view() {}
+    pub(crate) fn view(&self, theme: &Theme, font: Option<&Font>) {
+        set_fullscreen_camera();
+        let mut x = 0.0;
+        let y = screen_height();
 
-    fn parse_command(&self) -> Option<CommandUpdate> {
+        let dimensions = draw_text_ex("> ", x, y, TextParams {
+            color: theme.text,
+            font,
+            font_size: 16,
+            ..Default::default()
+        });
+
+        x += dimensions.width;
+
+        for (i, glyph) in self.text.chars().enumerate() {
+            if i == self.cursor {
+                draw_rectangle(x, y - 16.0, 2.0, 16.0, theme.rosewater);
+            }
+
+            let dimensions = draw_text_ex(&glyph.to_string(), x, y, TextParams {
+                color: theme.text,
+                font,
+                font_size: 16,
+                ..Default::default()
+            });
+
+            x += dimensions.width;
+
+        }
+        if self.cursor == self.text.len() + 1 {
+            draw_rectangle(x, y - 16.0, 2.0, 16.0, theme.rosewater);
+        }
+
+    }
+
+    fn parse_command(&self) -> Option<PromptUpdate> {
         let tokens: Vec<&str> = self.text.split(" ").collect();
         if tokens.len() < 1 {
             return None;
@@ -70,21 +108,21 @@ impl Command {
         match tokens[0] {
             "open" => {
                 if tokens.len() > 1 {
-                    Some(CommandUpdate::OpenFile(tokens[1].to_string()))
+                    Some(PromptUpdate::OpenFile(tokens[1].to_string()))
                 } else {
-                    Some(CommandUpdate::Status("Invalid command".to_string()))
+                    Some(PromptUpdate::Status("Invalid command".to_string()))
                 }
             }
-            "close" => Some(CommandUpdate::CloseActiveFile),
+            "close" => Some(PromptUpdate::CloseActiveFile),
             "save" => {
                 if tokens.len() > 1 {
-                    Some(CommandUpdate::SaveAs(tokens[1..].join(" ")))
+                    Some(PromptUpdate::SaveAs(tokens[1..].join(" ")))
                 } else {
-                    Some(CommandUpdate::SaveActiveFile)
+                    Some(PromptUpdate::SaveActiveFile)
                 }
             }
-            "help" => Some(CommandUpdate::OpenHelp),
-            _ => Some(CommandUpdate::Status("Invalid command".to_string()))
+            "help" => Some(PromptUpdate::OpenHelp),
+            _ => Some(PromptUpdate::Status("Invalid command".to_string()))
         }
     }
 }
